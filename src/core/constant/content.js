8894b271c06e4a6a047ef3cf99e9847dff89342f
Nguyễn Ngọc Thiện <91732816+ThienThomas@users.noreply.github.com>
2022-07-24 23:42:00 +0700

    1st commit

---
+++ src/core/constant/content.js
@@ -0,0 +1,11 @@
+export const CardContent = [
+    {img: require('/img/sp_1.jpg'), title: 'SF0013', content:'Ghế sofa mã SF0013 chính là sự lựa chọn hoàn hảo cho gia đình của bạn. Thiết kế tinh tế và sang trọng sẽ làm cho không gian của bạn trở nên độc đáo hơn'},
+    {img: require('/img/sp_2.jpg'), title: 'SF0013', content:'Ghế sofa mã SF0013 chính là sự lựa chọn hoàn hảo cho gia đình của bạn. Thiết kế tinh tế và sang trọng sẽ làm cho không gian của bạn trở nên độc đáo hơn'},
+    {img: require('/img/sp_3.jpg'), title: 'SF0013', content:'Ghế sofa mã SF0013 chính là sự lựa chọn hoàn hảo cho gia đình của bạn. Thiết kế tinh tế và sang trọng sẽ làm cho không gian của bạn trở nên độc đáo hơn'},
+    {img: require('/img/sp_4.jpg'), title: 'SF0013', content:'Ghế sofa mã SF0013 chính là sự lựa chọn hoàn hảo cho gia đình của bạn. Thiết kế tinh tế và sang trọng sẽ làm cho không gian của bạn trở nên độc đáo hơn'}
+  ]
+export const SectionContent = [
+    {img: require('/img/services_1.png'), title: 'Dịch vụ bảo hành', content:'Tại LKL PHUC THINH vai trò thiết kế kiến trúc nội thất của chúng tôi là mang lại thiết kế tinh tế và sang trọng và làm cho không gian của bạn trở nên độc đáo hơn'},
+    {img: require('/img/services_2.png'), title: 'Đội ngũ nhân lực', content:'Tại LKL PHUC THINH vai trò thiết kế kiến trúc nội thất của chúng tôi là mang lại thiết kế tinh tế và sang trọng và làm cho không gian của bạn trở nên độc đáo hơn'},
+    {img: require('/img/services_3.png'), title: 'Thiết kế nội thất', content:'Tại LKL PHUC THINH vai trò thiết kế kiến trúc nội thất của chúng tôi là mang lại thiết kế tinh tế và sang trọng và làm cho không gian của bạn trở nên độc đáo hơn'}, 
+]
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'naturebg' : "url('../img/nature.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
